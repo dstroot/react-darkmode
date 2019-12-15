ed08cf582ec30a7d435a4c39260df88c5e93ce0a
Dan Stroot <dan.stroot@gmail.com>
2019-12-15 09:12:52 -0800

    change event listener back - not supported in safari

--- src/hooks/useMediaQuery/index.js
+++ src/hooks/useMediaQuery/index.js
@@ -2,28 +2,40 @@ import { useState, useEffect } from "react";
 
 const useMediaQuery = query => {
   const [state, setState] = useState(false);
+  const supportMatchMedia =
+    typeof window !== "undefined" && typeof window.matchMedia !== "undefined";
 
   useEffect(() => {
     let mounted = true;
 
+    // This defensive check is here for simplicity.
+    if (!supportMatchMedia) {
+      return undefined;
+    }
+
     // Returns a MediaQueryList object
     const mql = window.matchMedia(query);
+
     const onChange = () => {
       if (!mounted) return;
       setState(!!mql.matches);
     };
 
-    mql.addEventListener(onChange);
+    mql.addListener(onChange); // addListener deprecated on most
+    // mql.addEventListener("change", onChange); // not supported by safari
     setState(mql.matches);
 
     // clean up
     return () => {
       mounted = false;
-      mql.removeEventListener(onChange);
+      mql.removeListener(onChange); // addListener deprecated on most
+      // mql.removeEventListener("change", onChange); // not supported by safari
     };
-  }, [query]);
+  }, [query, supportMatchMedia]);
 
   return state;
 };
 
 export default useMediaQuery;
+
+// https://material-ui.com/components/use-media-query/
